0f8364eb77e3749e85b72f9718fa5d7d1749de04
alekssf <alekss@alekss.lv>
2026-09-04 09:18:21 +0300

    Programma 1.0

---
+++ src/git1/Kaulins.java
@@ -0,0 +1,15 @@
+package git1;
+
+import java.util.Random;
+
+public class Kaulins {
+
+	public static void main(String[] args) {
+		int sk;
+		Random rand = new Random();
+		sk = rand.nextInt(6)+1;
+		System.out.println("Uzkrita skaitlis: "+sk);
+
+	}
+
+}
