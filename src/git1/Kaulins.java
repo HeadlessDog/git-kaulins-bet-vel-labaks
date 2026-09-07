99c9c10455b17f76f654663a63538ac5eed2c6a8
alekssf <alekss@alekss.lv>
2026-09-07 10:01:19 +0300

    For cikls

--- src/git1/Kaulins.java
+++ src/git1/Kaulins.java
@@ -4,15 +4,17 @@ import java.util.Random;
 
 public class Kaulins {
 
-	static void mestKaulinu() {
+	static void mestKaulinu(int reizes) {
 		int sk;
 		Random rand = new Random();
+		for(int i=0; i<reizes; i++) {
 		sk = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+sk);
+		}
 	}
 	
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(5);
 	}
 
 }
