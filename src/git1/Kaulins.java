d8d181cafa283284b914921361bfb9879b529853
alekssf <alekss@alekss.lv>
2026-09-07 10:07:25 +0300

    Ieviesta iespēja ievadīt reižu skaitu

--- src/git1/Kaulins.java
+++ src/git1/Kaulins.java
@@ -1,15 +1,21 @@
 package git1;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Kaulins {
 
 	public static void main(String[] args) {
-		int sk;
+		int sk, reizes;	
 		Random rand = new Random();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kauliņu?");
+		reizes = scan.nextInt();
+		for(int i=0; i<reizes; i++) {
 		sk = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+sk);
-
+		}
+		scan.close();
 	}
 
 }
