33558bcef7180793027adb3bdb663fb2215a03a6
alekssf <alekss@alekss.lv>
2026-09-07 09:55:45 +0300

    Ieviesta metode

--- src/git1/Kaulins.java
+++ src/git1/Kaulins.java
@@ -4,12 +4,15 @@ import java.util.Random;
 
 public class Kaulins {
 
-	public static void main(String[] args) {
+	static void mestKaulinu() {
 		int sk;
 		Random rand = new Random();
 		sk = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+sk);
-
+	}
+	
+	public static void main(String[] args) {
+		mestKaulinu();
 	}
 
 }
